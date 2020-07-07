1855622d375055e16967729fc201863e36e3beab
Denis Cherniy <d.chernyi@storytelling-software.com>
2020-07-07 15:55:05 +0300

    Created project.

---
+++ ToDo_SwiftUI/ToDo_SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ToDo_SwiftUI
+//
+//  Created by Denis Cherniy on 07.07.2020.
+//  Copyright © 2020 Denis Cherniy. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
