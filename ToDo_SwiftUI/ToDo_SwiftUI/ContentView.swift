679f8e2169ca4721fec076e59e297eac8fe66219
Denis Cherniy <d.chernyi@storytelling-software.com>
2020-07-15 14:12:13 +0300

    Implemented adding and deleting items.

--- ToDo_SwiftUI/ToDo_SwiftUI/ContentView.swift
+++ ToDo_SwiftUI/ToDo_SwiftUI/ContentView.swift
@@ -10,32 +10,97 @@ import SwiftUI
 
 struct ContentView: View {
     let databaseGateway: DataStoreGateway
-    @State var items: [ToDoItem]
-    @State private var showingDetail = false
+    @State var items: [ToDoItem] = []
+    @State private var showingDetail = false {
+        didSet {
+            if showingDetail == false {
+                itemNameToCreate = ""
+            }
+        }
+    }
+    @State private var itemNameToCreate = ""
+    
+    init(databaseGateway: DataStoreGateway) {
+        self.databaseGateway = databaseGateway
+    }
+    
+    private var createItemSection: some View {
+        HStack {
+            Button(action: {
+                // save item
+                self.createItem()
+                self.showingDetail = false
+            }) {
+                Image(systemName: "checkmark")
+            }
+            .foregroundColor(itemNameToCreate.count == 0 ? .gray : .green)
+            .disabled(itemNameToCreate.count == 0)
+            
+            TextField("Write a new item name...", text: $itemNameToCreate, onCommit: {
+                // save item
+                self.createItem()
+                self.showingDetail = false
+            })
+            .textFieldStyle(RoundedBorderTextFieldStyle())
+            
+            Button(action: {
+                self.showingDetail = false
+            }) {
+                Image(systemName: "xmark")
+            }
+            .foregroundColor(.red)
+        }.padding()
+    }
     
     var body: some View {
         NavigationView {
-            Form {
-                List(items) { item in
-                    ToDoRowView(item: item)
+            VStack {
+                if showingDetail {
+                    createItemSection
+                }
+                
+                Form {
+                    List {
+                        ForEach(items) { item in
+                            ToDoRowView(item: item)
+                        }
+                        .onDelete(perform: deleteItem(at:))
+                    }
+                }
+                .navigationBarTitle("ToDo", displayMode: .inline)
+                .navigationBarItems(leading: Button("Add") {
+                    self.showingDetail.toggle()
+                    print("*** Add tapped!")
+                }
+                .disabled(showingDetail))
+                .onAppear() {
+                    let fetchedItems = self.databaseGateway.fetchAll()
+                    self.items = fetchedItems
                 }
             }
-            .navigationBarTitle("ToDo", displayMode: .inline)
-            .navigationBarItems(leading: Button("Add") {
-                self.showingDetail.toggle()
-                print("*** Add tapped!")
-            }.sheet(isPresented: $showingDetail) {
-                ItemDetailView(createItemClosure: { createdItem in
-                    print("*** \(createdItem)")
-                })
-            })
         }
+    }
+}
+
+extension ContentView {
+    private func createItem() {
+        let item = ToDoItem(id: UUID().uuidString, name: itemNameToCreate)
+        items.append(item)
+        databaseGateway.insert(item: item)
+    }
+    
+    private func deleteItem(at offsets: IndexSet) {
+        let item = items[offsets.first!]
+        databaseGateway.delete(item: item)
+        items.remove(atOffsets: offsets)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView(databaseGateway: CoredataGateway(), items: [ToDoItem(id: "abc", name: "Finish App!"),
-                            ToDoItem(id: "abd", name: "Create Bindings", checked: true)])
+        let view = ContentView(databaseGateway: CoredataGateway())
+        view.items = [ToDoItem(id: "abc", name: "Finish App!"),
+        ToDoItem(id: "abd", name: "Create Bindings", checked: true)]
+        return view
     }
 }
